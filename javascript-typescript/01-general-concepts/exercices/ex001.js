e11afb73075c36ef06d646839f302b6df0162d87
Abner Landim <abnerlandim@macbookdeabner.lan>
2023-11-03 22:17:09 +0000

    ex001 js

---
+++ javascript-typescript/01-general-concepts/exercices/ex001.js
@@ -0,0 +1,30 @@
+const name = "Abner";
+
+console.log(name);
+
+const age = "28";
+
+console.log(age);
+
+const isEmploye = false;
+console.log(isEmploye);
+
+const trabalho = null;
+console.log(trabalho);
+
+const endereço = "";
+console.log(endereço);
+
+const nomeCompleto = "Abner Landim";
+console.log(nomeCompleto);
+
+const mensagem = "3 de Novembro, primeira aula de JavaScript";
+console.log(mensagem);
+
+const nomeCompletoMensagem = nomeCompleto + ", " + mensagem;
+console.log(nomeCompletoMensagem);
+
+const num1 = 12;
+const num2 = 10;
+const soma = num1 + num2;
+console.log(soma);
